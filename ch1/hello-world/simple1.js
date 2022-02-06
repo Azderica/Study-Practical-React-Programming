ea493ee5e8bb2fc53e08e5e57c07da30aa97b64c
Azderica <mh97888@gmail.com>
2022-02-06 23:12:30 +0900

    바벨 사용해보기

--- ch1/hello-world/simple1.js
+++ ch1/hello-world/simple1.js
@@ -1,27 +1,30 @@
 function LikeButton() {
   // 현재 상태를 liked라는 상탯값에 저장하고, setLiked로 상탯값을 변경합니다.
-  const [liked, setLiked] = React.useState(false)
-  const text = liked ? '좋아요 취소' : '좋아요'
-
-  // 리액트 요소를 반환, 리액트에서 UI를 표현하는 가장 작은 단위를 리액트 요소로 보면됨
+  const [liked, setLiked] = React.useState(false);
+  const text = liked ? '좋아요 취소' : '좋아요'; // 리액트 요소를 반환, 리액트에서 UI를 표현하는 가장 작은 단위를 리액트 요소로 보면됨
   // 첫번째 매개변수는 html 태그 요소, 두번째 매개변수는 속성값, 세번째 이후는 children
-  return <button onClick={() => setLiked(!liked)}>{text}</button>
+
+  return /*#__PURE__*/React.createElement("button", {
+    onClick: () => setLiked(!liked)
+  }, text);
 }
 
 function Container() {
-  const [count, setCount] = React.useState(0)
-  return (
-    <div>
-      <LikeButton />
-      <div style={{ marginTop: 20 }}>
-        <span>현재 카운트: </span>
-        <span style={{ marginRight: 20 }}></span>
-        <button onClick={() => setCount(count + 1)}>증가</button>
-        <button onClick={() => setCount(count - 1)}>감소</button>
-      </div>
-    </div>
-  )
+  const [count, setCount] = React.useState(0);
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(LikeButton, null), /*#__PURE__*/React.createElement("div", {
+    style: {
+      marginTop: 20
+    }
+  }, /*#__PURE__*/React.createElement("span", null, "\uD604\uC7AC \uCE74\uC6B4\uD2B8: "), /*#__PURE__*/React.createElement("span", {
+    style: {
+      marginRight: 20
+    }
+  }, count), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count + 1)
+  }, "\uC99D\uAC00"), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count - 1)
+  }, "\uAC10\uC18C")));
 }
 
-const domContainer = document.getElementById('root')
-ReactDOM.render(React.createElement(Container), domContainer)
+const domContainer = document.getElementById('root');
+ReactDOM.render(React.createElement(Container), domContainer);
--- ch1/hello-world/temp/jsx.js
+++
@@ -1,8 +0,0 @@
-// jsx는 다음의 형태로 입력할 수 있음
-<div className="box">
-  <Title text="hello world" width=(200) />
-  <button onClick={() => { }}>좋아요</button>
-  <a href="/home" style=({marginTop: '10px', color: 'red'})>
-  홈으로 이동
-</a>
-</div >
