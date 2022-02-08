9b6e45ba0e4a57778d6e15916df9e456edccb970
Azderica <mh97888@gmail.com>
2022-02-08 21:17:27 +0900

    ~create-react-app 시작하기 2

--- ch1/cra-test/src/index.js
+++ ch1/cra-test/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'core-js/features/string/pad-start'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,7 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+console.log(`process.env.NODE_ENV`, process.env.NODE_ENV)
+console.log(`process.env.REACT_APP_API_URL`, process.env.REACT_APP_API_URL)
+console.log(`process.env.REACT_APP_TEMP1`, process.env.REACT_APP_TEMP1)
